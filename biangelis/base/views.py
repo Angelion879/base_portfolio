411ab4797c94f22797555f0982035130bc1ee89f
Angelion879 <diangelis879@outlook.com>
2024-11-20 15:05:32 -0300

    (feat): create basic urls with test httpresponse views

--- biangelis/base/views.py
+++ biangelis/base/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('<h2>Home Page</h2>')
+
+def feed(request):
+    return HttpResponse('<h2>Posts feed</h2>')
+
+def post(request):
+    return HttpResponse('<h2>Post page</h2>')
+
+def profile(request):
+    return HttpResponse('<h2>Profile</h2>')
