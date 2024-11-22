573a5930fd2a60af10ffe133e1a2ff7ce230f8d2
Angelion879 <diangelis879@outlook.com>
2024-11-22 14:24:58 -0300

    (feat): static files basic django configuration

--- biangelis/biangelis/settings.py
+++ biangelis/biangelis/settings.py
@@ -122,6 +122,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/image/'
+
+STATICFILES_DIR = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+MEDIA_ROOT = os.path.join('static/image')
+STATIC_ROOT = os.path.join('staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
