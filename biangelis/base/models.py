651fb94f2b2d3f56b74e9eaa7bdd0232c5bb1abc
Angelion879 <diangelis879@outlook.com>
2024-12-10 11:46:38 -0300

    (feat): create post DB model

--- biangelis/base/models.py
+++ biangelis/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    sub_title = models.CharField(max_length=200, null=True, blank=True)
+    thumbnail = models.ImageField(null=True, blank=True, upload_to="image", default="placeholder.png")
+    body = models.TextField(null=True, blank=True)
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
