5baeb980146022c34497b60d336df558a14faa00
Angelion879 <diangelis879@outlook.com>
2024-12-13 12:00:34 -0300

    (feat): add decorative icons

--- biangelis/base/views.py
+++ biangelis/base/views.py
@@ -4,7 +4,10 @@ from .models import Post
 # Create your views here.
 
 def home(request):
-    return render(request, 'basis/home.html')
+    context = {
+        "repeat" : [1,2,3,4,5,6,7,8,9,10,12,13,14,15],
+    }
+    return render(request, 'basis/home.html', context)
 
 def feed(request):
     posts = Post.objects.filter(active=True)
