611160c6371a60fa660b867e2fc0428db0fabbbf
Angelion879 <diangelis879@outlook.com>
2025-01-07 13:58:49 -0300

    (feat) add create_post url

--- biangelis/base/urls.py
+++ biangelis/base/urls.py
@@ -6,4 +6,8 @@ urlpatterns =[
     path('feed/', views.feed, name="feed"),
     path('post/', views.post, name="post"),
     path('contact/', views.contact, name="contact"),
+
+    # CRUD paths
+    
+    path('create_post/', views.createPost, name="create_post"),
 ]
