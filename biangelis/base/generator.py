6cd1b8afc7ae9245c8448dab3c401b63d4319768
Angelion879 <diangelis879@outlook.com>
2025-01-13 17:42:49 -0300

    (feat) icon wallpaper generation

---
+++ biangelis/base/generator.py
@@ -0,0 +1,14 @@
+
+def home_repeat():
+    nums = []
+    for i in range(1,15,+1):
+        nums.append(i)
+
+    return nums
+
+def post_repeat():
+    nums = []
+    for i in range(1,20,+1):
+        nums.append(i)
+
+    return nums
