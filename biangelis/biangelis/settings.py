966cb097b6b1c4e66ca55812e7460827819833a5
Angelion879 <diangelis879@outlook.com>
2025-01-16 19:59:40 -0300

    (feat) gcp database setup

--- biangelis/biangelis/settings.py
+++ biangelis/biangelis/settings.py
@@ -94,8 +94,11 @@ WSGI_APPLICATION = 'biangelis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'HOST': os.getenv('BD_ADDRESS'),
+        'USER': os.getenv('BD_USERNAME'),
+        'PASSWORD': os.getenv('BD_PASSWORD'),
+        'NAME': 'portfolio',
     }
 }
 
