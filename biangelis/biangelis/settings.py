8847d7310a7b2fa925d0428ecd7e3ade25da674f
Angelion879 <diangelis879@outlook.com>
2025-01-16 23:59:34 -0300

    (test) comment out local gs auth config

--- biangelis/biangelis/settings.py
+++ biangelis/biangelis/settings.py
@@ -174,9 +174,11 @@ GS_BUCKET_NAME = os.getenv('BKT_NAME')
 # Add an unique ID to a file name if same file name exists
 GS_FILE_OVERWRITE = False
 
+'''
 GS_CREDENTIALS = service_account.Credentials.from_service_account_file(
     os.path.join(BASE_DIR, 'gcp.json'),
 )
+'''
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
